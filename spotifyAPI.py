d4747bce0414e45deb3dbfe86dbaea672730f1f8
Roger Barber <rebarber@umich.edu>
2022-11-11 12:53:46 -0500

    Figuring out how to pull track info and making function.

--- spotifyAPI.py
+++ spotifyAPI.py
@@ -8,9 +8,25 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 # Creating spotipy object with credentials
 client_credentials_manager = SpotifyClientCredentials(client_id="670aabd450884ac4b78a2cdfcc6efb9e", client_secret="3c6bfedf6ddd4a579cd735ad2bd8b6d6")
+# THIS IS YOUR OBJECT BELOW
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-print(sp)
+
+# Grabbing track info from playlist stored above (grabbed from Spotify app)
+tracklist = sp.playlist_tracks("6UeSakyzhiEt4NB3UAd6NQ")
+#Structure is dict -> list -> dict -> list -> dict -> list for track URI
+
+#below grabs track name
+print(tracklist['items'][0]['track']['name'])
+#Below grabs artist name from track
+print(tracklist['items'][0]['track']['artists'][0]['name'])
+#Below finds track URI successsfully
+sample_uri = tracklist['items'][0]['track']['uri']
+
+#writing function to grab info and generate dictionary from above information
+
+def spotipyScouring(sp_object):
+    pass
 
 
 
