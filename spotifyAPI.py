4c58a00cf883dedb6fea3ebdc74699c01dce1710
Roger Barber <rebarber@umich.edu>
2022-11-11 11:56:55 -0500

    Added playlist id.

--- spotifyAPI.py
+++ spotifyAPI.py
@@ -1,6 +1,8 @@
 # Client ID: 670aabd450884ac4b78a2cdfcc6efb9e
 # Client Secret: 3c6bfedf6ddd4a579cd735ad2bd8b6d6
 
+# Billboard Top 100 Playlist ID: 6UeSakyzhiEt4NB3UAd6NQ
+
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
